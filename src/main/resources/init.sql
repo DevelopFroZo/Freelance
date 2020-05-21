4dddda8398acb3e7007b954d53b672fc1427dd28
FroZo <frozo02@yandex.ru>
2020-05-21 11:05:31 +0800

    prepare to merge

---
+++ src/main/resources/init.sql
@@ -0,0 +1,16 @@
+-- Add categories
+insert into categories( name )
+values
+    (  );
+
+-- Add docs
+
+-- Add payment methods
+
+-- Add users
+
+-- Add executor rating
+
+-- Add tasks
+
+-- Add solutions
